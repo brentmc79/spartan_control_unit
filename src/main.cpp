caec2fa1666bf81b7333ddb8c85d975197700898
Brent Collier <brentmc79@gmail.com>
2025-12-27 22:27:02 -0800

    Clean up main.cpp
    - Refactor setup code, extracting logic into functions specific to
      device type or responsibility
    - Remove any unused or commented-out code

--- src/main.cpp
+++ src/main.cpp
@@ -33,19 +33,12 @@ Adafruit_ST7789 tft = Adafruit_ST7789(LCD_CS, LCD_DC, LCD_RST);
 Adafruit_NeoPixel pixels(NUM_LEDS, LED_DATA, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel onboardLED(1, 48, NEO_GRB + NEO_KHZ800);
 
-OneButton buttonOne = OneButton(
-  BUTTON_1,  // Input pin for the button
-  true,      // Button is active LOW
-  true       // Enable internal pull-up resistor
-);
-OneButton buttonTwo = OneButton(
-  BUTTON_2,  // Input pin for the button
-  true,      // Button is active LOW
-  true       // Enable internal pull-up resistor
-);
+OneButton buttonOne = OneButton(BUTTON_1, true, true);
+OneButton buttonTwo = OneButton(BUTTON_2, true, true);
 
 uint8_t sendAddress[] = {0x44, 0x17, 0x93, 0x6C, 0x62, 0x84}; // esp32 1.9 in tft lcd
 uint8_t recvAddress[] = {0x24, 0x58, 0x7C, 0xDB, 0x31, 0x3C}; // esp32 s3 super mini
+uint8_t (*peerAddress)[6] = NULL;
 
 typedef struct struct_message {
     char msg[32];
@@ -61,7 +54,6 @@ void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
   Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
 
-bool pixelsOn = false;
 // Callback when data is received
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
   memcpy(&incomingReadings, incomingData, sizeof(incomingReadings));
@@ -71,27 +63,9 @@ void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
   Serial.println(incomingReadings.msg);
   Serial.print("Value from Peer: ");
   Serial.println(incomingReadings.val);
-
-  if (pixelsOn) {
-    pixels.clear();
-    pixelsOn = false;
-  } else {
-    pixels.setPixelColor(0, pixels.Color(0, 0, 255));
-    pixels.setBrightness(255);
-    pixelsOn = true;
-  }
-  pixels.show();
-
-  onboardLED.setBrightness(50);
-  onboardLED.setPixelColor(0, onboardLED.Color(0, 0, 255));
-  onboardLED.show();
-  delay(250);
-  onboardLED.clear();
-  onboardLED.show();
 }
 
-// Handler function for a single click:
-static void handleClick() {
+void pingReceiver() {
   Serial.println("Clicked!");
   tft.fillScreen(ST77XX_BLACK);
   tft.setCursor(10, 10);
@@ -119,46 +93,11 @@ static void handleClick() {
   }
 }
 
-void setup() {
-  Serial.begin(115200);
+// Handler function for a single click:
+static void handleClick() {
+}
 
-  if(!isReceiver){
-    onboardLED.begin();
-    onboardLED.clear();
-    //onboardLED.setPixelColor(0, onboardLED.Color(0, 0, 255));
-    //onboardLED.setBrightness(10);
-    onboardLED.show();
-  }
-
-  Serial.println("Fetching MAC address...");
-  macAddress = WiFi.macAddress();
-  Serial.print(macAddress);
-
-  //pinMode(BUTTON_1, INPUT_PULLUP);
-  //pinMode(BUTTON_2, INPUT_PULLUP);
-  //pinMode(FAN_1_CTRL, OUTPUT);
-  delay(100); // Allow pins to settle
-
-  pixels.begin();
-  pixels.clear();
-  pixels.show();
-
-  if (isInterface) {
-    tft.init(170, 320);
-    tft.setRotation(1);
-    tft.setTextSize(2);
-    tft.setTextColor(ST77XX_WHITE);
-    tft.fillScreen(ST77XX_BLACK);
-    tft.setCursor(0, 0);
- 
-    // Single Click event attachment
-    buttonOne.attachClick(handleClick);
-  }
-
-  if (VERIFY_HARDWARE) {
-    verifyHardwareConnections(tft, pixels);
-  }
-
+void setupEspComms() {
   WiFi.mode(WIFI_STA);
 
   // Init ESP-NOW
@@ -173,10 +112,7 @@ void setup() {
 
   // Register peer
   esp_now_peer_info_t peerInfo = {};
-  if (isInterface)
-    memcpy(peerInfo.peer_addr, recvAddress, 6);
-  else
-    memcpy(peerInfo.peer_addr, sendAddress, 6);
+  memcpy(peerInfo.peer_addr, peerAddress, 6);
   peerInfo.channel = 0;  
   peerInfo.encrypt = false;
   
@@ -185,29 +121,66 @@ void setup() {
     return;
   }
 
-  //scrollLogo(&tft);
-  //loadingAnimation2(&tft);
-  //renderHaloLoadingAnimation(&tft);
-  //loadingAnimation(&tft);
-
   Serial.print("Awaiting messages at ");
   Serial.println(macAddress);
+}
+
+// Execute any setup that is specific to the receiver device
+void setupReceiver() {
+  peerAddress = &sendAddress;
+
+  onboardLED.begin();
+  onboardLED.clear();
+  onboardLED.show();
+
+  pixels.begin();
+  pixels.clear();
+  pixels.show();
+
+  pinMode(FAN_1_CTRL, OUTPUT);
+  delay(100); // Allow pins to settle
+}
+
+// Execute any setup that is specific to the interface device
+void setupInterface() {
+  peerAddress = &recvAddress;
+
+  tft.init(170, 320);
+  tft.setRotation(1);
+  tft.setTextSize(2);
+  tft.setTextColor(ST77XX_WHITE);
+  tft.fillScreen(ST77XX_BLACK);
+  tft.setCursor(0, 0);
+
+  // Single Click event attachment
+  buttonOne.attachClick(handleClick);
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  Serial.println("Fetching MAC address...");
+  macAddress = WiFi.macAddress();
+  Serial.print(macAddress);
+
+  setupEspComms();
+
+  if (isReceiver) {
+    setupReceiver();
+  }
+
+  if (isInterface) {
+    setupInterface();
+  }
+
+  if (VERIFY_HARDWARE) {
+    verifyHardwareConnections(tft, pixels);
+  }
+
+  // TODO: render loading animation or scroll logo
 }
 
 void loop() {
   buttonOne.tick();
   buttonTwo.tick();
-
-  //tft.println("loop");
-  if (isInterface) {
-    //delay(1000);
-    //tft.fillScreen(ST77XX_BLACK);
-    //delay(8000);
-  } else {
-    //Serial.print("Awaiting messages at ");
-    //Serial.println(macAddress);
-  }
-  //delay(5000);
-  //Serial.println("loop");
-  // Your main program loop
 }
